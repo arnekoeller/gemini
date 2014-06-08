644870c44a5eaea8601d6aea6c4a4fb6174f1790
arnekoeller <arne.koeller@gmail.com>
2014-06-08 17:35:25 +0200

    Changed gemini.py to make it more readable, relieable and easier to use

--- src/gemini/gemini.py
+++ src/gemini/gemini.py
@@ -1,326 +1,301 @@
 import os,sys,socket,threading,time,Queue
 from struct import *
 
-class Gemini(object):
-
-	def __init__(self,serverAddr):
-		# serverAddr -> (IP,Port)
-		self.serverAddr = serverAddr
-
-	def getServerAddr(self):
-		return self.serverAddr
-
-	def checkRoot(self):
-		if (os.getuid()!= 0):
-			print "Not root rights"
-			sys.exit()
-
-	def checkOSType(self):
-		if sys.platform == "linux" or sys.platform == "linux2":
-			return "Linux"
-		elif sys.platform == "darwin":
-			return "MacOSX"
-		elif sys.platform == "win32":
-			return "Windows"
-
-	def startClient(self):
-
-		try:
-			clientfd = socket.socket(socket.AF_INET,socket.SOCK_DGRAM,0); 
-			#like default parameters
-			clientfd.connect(self.serverAddr)
-
-		except socket.error ,msg:
-				print "Cannot create socket to server" 
-				print msg
-
-		return clientfd
-
-	def sendMessageToServer(self,clientfd,message):
-		clientfd.sendto(message,self.serverAddr)
-		
-
-	def recvMessageFromServer(self,clientfd):
-		try:
-			recv = clientfd.recvfrom(2048)
-			return recv[0]
-		except socket.error:
-			print "Server might not be up"
-
-			#[0] has reply [1] is none
-
-	def closeClient(self,clientfd):
-			clientfd.close()
 
 
-class Sniffer(object):
-
-	def __init__(self,clientfd,mygemini,modus,queue):
-		self.clientfd = clientfd
-		self.mygemini = mygemini
-		self.modus = modus #off-> start sniffing off on-> start sniffing on
-		self.queue = queue
-
-	def setModus(self,modus):
-		self.modus = modus # modus either true or false
-
-	def createRawSocket(self):	
-
-		try:
-			rawfd = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs((0x0003)))
-			self.rawfd = rawfd
-		except socket.error ,msg:
-			print "Cannot create raw socket"
-			print " -> " + str(msg)
-		return rawfd
-
-
-	def startSniffing(self):
-		
-		while self.modus == True:
-			#print self.modus
-			packet = self.rawfd.recvfrom(2048)
-			packet = packet[0]
-			if self.sortOutPackets(packet,self.mygemini) == True:
-				#self.mygemini.sendMessageToServer(self.clientfd,packet)
-				self.queue.put(packet)
-
-	def stopSniffing(self):
-		self.rawfd.close()
-		
-
-	# Sorting out packets to/from localhost, to Server IP or if IPv6 is in use
-	def sortOutPackets(self,packet,mygemini):
-
-		eth_length = 14
-		eth_header = packet[:eth_length]
-		eth = unpack('!6s6sH' , eth_header)
-		eth_protocol = socket.ntohs(eth[2])
-
-		if eth_protocol == 8 :
-
-				ip_header = packet[eth_length:20+eth_length]
-
-				iph = unpack('!BBHHHBBH4s4s' , ip_header)
-
-				version_ihl = iph[0]
-				version = version_ihl >> 4
-				ihl = version_ihl & 0xF
-				iph_length = ihl * 4
-
-				# no support for IPv6
-				if version == 6:
-					return False
-
-				s_addr = socket.inet_ntoa(iph[8]);
-				d_addr = socket.inet_ntoa(iph[9]);
-
-				if(str(s_addr) == "127.0.0.1" or str(d_addr) == "127.0.0.1"):
-					return False
-				if(str(d_addr) == (mygemini.getServerAddr()[0]) or str(s_addr) == (mygemini.getServerAddr()[0]) ):
-					return False
-
+def checkRoot():
+	if (os.getuid()!= 0):
+		return False
+	else:
 		return True
 
-class GeminiMainThread(threading.Thread):
-
-	def __init__(self, threadId, threadName, serverAddr = None ):
-
-		#Checks if thread is of GeminiMainThread Class Type and not Subclass type
-		#if so, thread is being initialized within the subclass 
-		if serverAddr != None:
-			threading.Thread.__init__(self)
-			print "Gemini Main Thread has been successfully initialized ..."
-
-		self.threadId = threadId
-		self.threadName = threadName
-
-		self.serverAddr = serverAddr
-			
-
-	def run(self):
-
-		gemini  = Gemini(self.serverAddr)
-		self.geminiClassObj = gemini 	#reference to Gemini Object
-		clientfd  = gemini.startClient() 
-		self.geminiSocketClientFd = clientfd #returned FD from socket to server
+def checkOSType():
+	if sys.platform == "linux" or sys.platform == "linux2":
+		return "Linux"
+	elif sys.platform == "darwin":
+		return "MacOSX"
+	elif sys.platform == "win32":
+		return "Windows"
 
 
-class GeminiDelegatorThread(GeminiMainThread):
+def startClient(serverAddr):
 
-	def __init__(self, threadId, threadName, threadtype, mainthread , queue, runmodus = True ):
+	try:
 
-		super(GeminiDelegatorThread,self).__init__(threadId, threadName, None)
+		clientfd = socket.socket(socket.AF_INET,socket.SOCK_DGRAM,0); 
+		clientfd.connect(serverAddr)
+
+	except socket.error ,msg:
+		print "[-] Cannot create socket to server" 
+		print "    -> " + str(msg)
+
+	return clientfd
+
+def closeClient(clientfd):
+	clientfd.close()
+
+def sendMessageToServer(clientfd,message,serverAddr):
+	try:
+		clientfd.sendto(message,serverAddr)
+	except socket.error,msg:
+		#print "[!] Error:" + str(msg)
+		#error already handled
+		pass
+
+def recvMessageFromServer(clientfd):
+
+	try:
+		recv = clientfd.recvfrom(2048)
+		return recv[0]
+	except socket.error:
+		return "SERVERNOTUP"
+
+
+	
+
+def createRawSocket():	
+
+	try:
+		rawfd = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs((0x0003)))
+	except socket.error ,msg:
+		print "[-] Cannot create raw socket"
+		print "    -> " + str(msg)
+
+	return rawfd
+
+def sniff(rawfd,queue,serverAddr):
+
+	packet = rawfd.recvfrom(2048)
+	packet = packet[0]
+	if sortOutPackets(packet,serverAddr) == True:
+		queue.put(packet)
+
+def stopSniffing(rawfd):
+	rawfd.close()
+
+def sortOutPackets(packet,serverAddr):
+
+	eth_length = 14
+	eth_header = packet[:eth_length]
+	eth = unpack('!6s6sH' , eth_header)
+	eth_protocol = socket.ntohs(eth[2])
+
+	if eth_protocol == 8 :
+
+		ip_header = packet[eth_length:20+eth_length]
+
+		iph = unpack('!BBHHHBBH4s4s' , ip_header)
+
+		version_ihl = iph[0]
+		version = version_ihl >> 4
+		ihl = version_ihl & 0xF
+		iph_length = ihl * 4
+
+		# no support for IPv6
+		if version == 6:
+			return False
+
+		s_addr = socket.inet_ntoa(iph[8]);
+		d_addr = socket.inet_ntoa(iph[9]);
+
+		if(str(s_addr) == "127.0.0.1" or str(d_addr) == "127.0.0.1"):
+			return False
+		if(str(d_addr) == serverAddr[0] or str(s_addr) == serverAddr[0] ):
+			return False
+
+	return True
+
+
+
+class GeminiDelegatorThread(threading.Thread):
+
+	def __init__(self, threadId, queue, serverAddr, threadlist , runmodus = True ) :
 
 		threading.Thread.__init__(self)
 
-		self.threadtype = threadtype
-		self.mainthread = mainthread
-		self.runmodus = runmodus
+		self.id = id
 		self.queue = queue
+		self.serverAddr = serverAddr
+		self.runmodus = runmodus
+		self.threadlist = threadlist
 
+		self.server_state = "DOWN"
 		self.mySniffer = None
 
-		print "Delegator Thread has been successfully initialized ..."
+		print "[+] Delegator Thread has been successfully initialized "
 
 	def run(self):
 
-		try:
-			myGemini = self.mainthread.geminiClassObj
-			#print myGemini
-			myGeminiClientFd = self.mainthread.geminiSocketClientFd
-			#print myGeminiClientFd
-		except AttributeError,msg:
-			time.sleep(2)
-			myGeminiClientFd = self.mainthread.geminiSocketClientFd
-			myGemini = self.mainthread.geminiClassObj
+		self.gsockfd = startClient(self.serverAddr)
 
-		print "Delegator Thread started listening ..."
+		print "[!] Delegator Thread started listening for Server " + str(self.serverAddr) + " to communicate"
+
+		self.queue.put("HIYA")	#start Handshake
 
 		while self.runmodus:
-			
-			data = myGemini.recvMessageFromServer(myGeminiClientFd)
-			print data
+
+			data = recvMessageFromServer(self.gsockfd)
+			#print data
+			if data == "SERVERNOTUP":
+				print "[!] Server is probably not up. Trying to contact server again in 30s"
+				time.sleep(30)	# wait for 30secs and try to contact server again
+				self.queue.put("HIYA")
+				self.server_state = "DOWN"
+				continue
 			if data == "ISEEU":
-				#print "he said bye"
+				# confirmation that handshake succeeded
+				self.server_state = "UP"
 				pass
 			if data == "STARTSNIFFING":
 
-				mySniffer = GeminiSnifferThread(4,"SnifferThread","Sniffer",self.mainthread,self.queue )
+				mySniffer = GeminiSnifferThread(3, self.queue, self.serverAddr)
 				self.mySniffer = mySniffer
 				mySniffer.start()
+				self.threadlist.append(mySniffer)
 
 			if data == "STOPSNIFFING":
 
 				if self.mySniffer != None:
 					self.mySniffer.stop()
 
-			if data == "BYE":
-				print "Shutting everything down"
-				print threading.enumerate()
+			if data == "GEMINISHUTDOWN":
+				print "[!] Shutting everything down. Please wait ..."
+
+				# Turn off Sniffer
+				if self.mySniffer != None:
+					self.mySniffer.stop()
+
+				# Send command to Queue so Transmitter shuts down
 				self.queue.put("GEMINISHUTDOWN")
-				
+
+				# Turn Delegator off
 				self.stop()
 
-			#print threading.enumerate()
 
 	def stop(self):
-		self.mainthread.geminiClassObj.closeClient(self.mainthread.geminiSocketClientFd)
+		print "[!] Stopping Delegator"
+		time.sleep(3)
+		closeClient(self.gsockfd)
 		self.runmodus = False
-		time.sleep(2)
-		try:
-			self.join()
-		except RuntimeError, msg:
-			print msg
+		time.sleep(1)
 
 
-class GeminiTransmitterThread(GeminiMainThread):
+class GeminiTransmitterThread(threading.Thread):
 
-	def __init__(self, threadId, threadName, threadtype, mainthread, queue, runmodus = True):
-
-		super(GeminiTransmitterThread,self).__init__(threadId, threadName, None)
+	def __init__(self, threadId , queue, gsockfd, serverAddr, runmodus = True):
 
 		threading.Thread.__init__(self)
 
-		self.threadtype = threadtype
-		self.mainthread = mainthread
+		self.threadId = threadId
+		self.gsockfd = gsockfd
+		self.serverAddr = serverAddr
 		self.runmodus = runmodus
 		self.queue = queue
 
-		print "Transmitter Thread has been successfully initialized ..."
+		print "[+] Transmitter Thread has been successfully initialized "
 
 	def run(self):
 
-		self.initializeHandshakeToServer()
+		print "[!] Trying to contact Server at " + str(self.serverAddr)
+
 		while self.runmodus == True:
 			while not self.queue.empty():
 				data = self.queue.get()
 				if data == "GEMINISHUTDOWN":
 					self.stop()
 				else:
-					self.sendMessageToServer(data)
+					sendMessageToServer(self.gsockfd,data,self.serverAddr)
 
-
-	def initializeHandshakeToServer(self):
-		#print self.mainthread.serverAddr
-		self.mainthread.geminiSocketClientFd.sendto("HIYA",self.mainthread.serverAddr)
-
-	def sendMessageToServer(self,message):
-		self.mainthread.geminiSocketClientFd.sendto(message,self.mainthread.serverAddr)
 
 	def stop(self):
+		print "[!] Stopping Transmitter "
 		self.runmodus = False
-		time.sleep(2)
-		try:
-			self.join()
-		except RuntimeError, msg:
-			print msg
+		time.sleep(1)
 
 
-class GeminiSnifferThread(GeminiMainThread):
 
-	def __init__(self, threadId, threadName, threadtype, mainthread, queue):
+class GeminiSnifferThread(threading.Thread):
 
-		super(GeminiSnifferThread,self).__init__(threadId, threadName)
+	def __init__(self, threadId, queue, serverAddr, runmodus = True):
 
 		threading.Thread.__init__(self)
 
-		self.threadtype = threadtype
-		self.mainthread = mainthread
+		self.threadId = threadId
 		self.queue = queue
+		self.runmodus = runmodus
+		self.serverAddr = serverAddr
 
+		self.rawfd = None
 
-		print "Sniffer Thread has been successfully initialized ..."
+		print "[+] Sniffer Thread has been successfully initialized "
 
 	def run(self):
 
-		mysniffer = Sniffer(self.mainthread.geminiSocketClientFd,self.mainthread.geminiClassObj,True,self.queue)
-		self.mysniffer = mysniffer
-		rawfd = mysniffer.createRawSocket()
-	 	mysniffer.startSniffing()
+	 	rawfd = createRawSocket()
+	 	self.rawfd = rawfd
+
+	 	print "[~] Sniffing ..."
+	 	while self.runmodus == True:
+	 		sniff(rawfd,self.queue,self.serverAddr)
 
 	def stop(self):
-		self.mysniffer.stopSniffing()
-		self.mysniffer.modus = False
-		time.sleep(2)
-		try:
-			self.join()
-		except RuntimeError, msg:
-			print msg
+		print "[!] Stopping Sniffer"
+		self.runmodus = False
+		stopSniffing(self.rawfd)
+		time.sleep(1)
+
 
 
 def main():
+
+	########################################## Start Screen #########################################
+
+	os.system("clear")	#clear screen
 	
+	print """                           _____                      _           _ 
+  			  / ____|                    (_)         (_)
+			 | |  __    ___   _ __ ___    _   _ __    _ 
+			 | | |_ |  / _ \ | '_ ` _ \  | | | '_ \  | |
+			 | |__| | |  __/ | | | | | | | | | | | | | |
+			  \_____|  \___| |_| |_| |_| |_| |_| |_| |_| arne.koeller@gmail.de"""
+	print "\t\t\t   Network Forensic Toolkit"
+	print "\n\n\n"
+	print "[+] Starting Gemini Client  "
+
+	#################################################################################################
+
+	if checkRoot() == False:
+		print "[!] You need to have root user rights to run Gemini \n\n\n"
+		sys.exit()
+	if checkOSType() != "Linux":
+		print "[!] You need to run this program on a *nix System\n\n\n"
+		sys.exit()
+
 	serverAddr = ("127.0.0.1",45681)
 	threadlist = []
 
 	transmitqueue = Queue.Queue()
 
-	mainthread = GeminiMainThread(1,"GeminiThread",serverAddr)
-	mainthread.start()
-	threadlist.append(mainthread)
+	time.sleep(1)
 
-
-	transmitter = GeminiTransmitterThread(2,"TransmitterThread","transmitter",mainthread , transmitqueue , True )
-	transmitter.start()
-	threadlist.append(transmitter)
-
-	delegator = GeminiDelegatorThread(3,"DelegatorThread","delegator",mainthread, transmitqueue , True )
+	delegator = GeminiDelegatorThread(1, transmitqueue , serverAddr, threadlist, True )
 	delegator.start()
 	threadlist.append(delegator)
 
+	time.sleep(3)
+	
+	transmitter = GeminiTransmitterThread(2, transmitqueue , delegator.gsockfd, serverAddr, True )
+	transmitter.start()
+	threadlist.append(transmitter)
 
+	
 
+	# Join all threads 
+	for thr in threadlist:
+		thr.join()
 
-	# try:
-	# 	mysniffer = Gemini.Sniffer(clientfd,mygemini)
-	# 	rawfd = mysniffer.createRawSocket()
-	# 	mysniffer.startSniffing(rawfd)
-	# except socket.error:
-	# 	print "oops"
-
-
-	# mygemini.closeClient(clientfd)
+	print "[+] Thank you for using Gemini"
+ 
 
 
 
--- src/gemini_server/gemini_server.py
+++ src/gemini_server/gemini_server.py
@@ -1,4 +1,4 @@
-import socket,sys           
+import socket,sys,time           
 from struct import *
 
 
@@ -44,6 +44,15 @@ class Gemini_Server:
 		self.clientaddr = recv[1]
 		return recv[0]	#[0] has reply [1] is none
 
+	def atohex(self,string):
+		hexstr = ""
+		for i in range(0,len(string)):
+			if ((i%8 == 0) and (i!=0) ):
+				hexstr += "\n"
+			hexstr += str(string[i].encode("hex")) + " "
+
+		return hexstr
+
 
 	def processData(self,packet):
 
@@ -62,59 +71,126 @@ class Gemini_Server:
 
 		packet_content = self.buildMac(packet[0:6]) + \
 						  self.buildMac(packet[6:12]) + str(eth_protocol)
-		print packet_content
+		print eth_protocol
 
 		if eth_protocol == 8:
-					
-					ip_header = packet[eth_length:20+eth_length]
+			#print "--------------------------------------------"
+			ip_header = packet[eth_length:20+eth_length]
 
-					iph = unpack('!BBHHHBBH4s4s' , ip_header)
+			iph = unpack('!BBHHHBBH4s4s' , ip_header)
 
-					version_ihl = iph[0]
-					version = version_ihl >> 4
-					ihl = version_ihl & 0xF
-					iph_length = ihl * 4
+			version_ihl = iph[0]
+			version = version_ihl >> 4
+			ihl = version_ihl & 0xF
+			iph_length = ihl * 4
 
-					tos = iph[1]
-					tot_len = iph[2]
-					ip_id = iph[3]
-					
-					frag = iph[4]
+			tos = iph[1]
+			tot_len = iph[2]
+			ip_id = iph[3]
+			
+			frag = iph[4]
 
-					ttl = iph[5]
-					protocol = iph[6]
-					cksm = iph[7]
-					s_addr = socket.inet_ntoa(iph[8]);
-					d_addr = socket.inet_ntoa(iph[9]);
+			ttl = iph[5]
+			protocol = iph[6]
+			cksm = iph[7]
+			s_addr = socket.inet_ntoa(iph[8]);
+			d_addr = socket.inet_ntoa(iph[9]);
 
-					# print "iphlength " + str(iph_length)
-					# print tos
-					# print "total length " + str(tot_len)
-					# print ip_id
-					# print ttl
-					# print "checksum " + str(cksm)
-					# print s_addr
+			# print "iphlength " + str(iph_length)
+			# print tos
+			# print "total length " + str(tot_len)
+			# print ip_id
+			# print ttl
+			# print "checksum " + str(cksm)
+			# print s_addr
 
-					if protocol == 6 :
+			#TCP
+			if protocol == 6 :
 
-						t = iph_length + eth_length
-						tcp_header = packet[t:t+20]
+				t = iph_length + eth_length
+				tcp_header = packet[t:t+20]
 
-						tcph = unpack('!HHLLBBHHH' , tcp_header)
+				tcph = unpack('!HHLLBBHHH' , tcp_header)
 
-						source_port = tcph[0]
-						dest_port = tcph[1]
-						sequence = tcph[2]
-						acknowledgement = tcph[3]
-						doff_reserved = tcph[4]
-						tcph_length = doff_reserved >> 4
+				source_port = tcph[0]
+				dest_port = tcph[1]
+				sequence = tcph[2]
+				acknowledgement = tcph[3]
+				doff_reserved = tcph[4]
+				tcph_length = doff_reserved >> 4
 
-						h_size = eth_length + iph_length + (tcph_length * 4)
-						data_size = len(packet) - h_size
+				h_size = eth_length + iph_length + (tcph_length * 4)
+				data_size = len(packet) - h_size
 
-						data = packet[h_size:]
-						print data
+				data = packet[h_size:]
 
+				print "TCp"
+				print "+++++++++++++++++++++++++++++"
+				print self.atohex(data)
+				print "+++++++++++++++++++++++++++++"
+
+			#ICMP Packets
+			elif protocol == 1 :
+				u = iph_length + eth_length
+				icmph_length = 4
+				icmp_header = packet[u:u+4]
+
+				#now unpack them :)
+				icmph = unpack('!BBH' , icmp_header)
+
+				icmp_type = icmph[0]
+				code = icmph[1]
+				checksum = icmph[2]
+
+				#print 'Type : ' + str(icmp_type) + ' Code : ' + str(code) + ' Checksum : ' + str(checksum)
+
+				h_size = eth_length + iph_length + icmph_length
+				data_size = len(packet) - h_size
+
+				#get data from the packet
+				data = packet[h_size:]
+
+				print "ICMP"
+				print "----------------------------------"
+				print self.atohex(data)
+				print "----------------------------------"
+
+			#UDP packets
+			elif protocol == 17 :
+				u = iph_length + eth_length
+				udph_length = 8
+				udp_header = packet[u:u+8]
+
+				#now unpack them :)
+				udph = unpack('!HHHH' , udp_header)
+
+				source_port = udph[0]
+				dest_port = udph[1]
+				length = udph[2]
+				checksum = udph[3]
+
+				print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Length : ' + str(length) + ' Checksum : ' + str(checksum)
+
+				h_size = eth_length + iph_length + udph_length
+				data_size = len(packet) - h_size
+
+				#get data from the packet
+				data = packet[h_size:]
+
+				print "UDP"
+				print "###################################"
+				print self.atohex(data)
+				print "###################################"
+
+			#some other IP packet like IGMP
+			else :
+				#print 'Protocol other than TCP/UDP/ICMP'
+				pass
+
+		#ARP
+		if eth_protocol == 1544:
+
+			print "--> --> --> ARP <-- <-- <--"
 
 		
 
@@ -128,8 +204,8 @@ class Gemini_Server:
 			serverfd.close()
 
 	def serverClosedownHandshake(self,serverfd):
-		self.sendMessageToClient(serverfd,"STOPSNIFFING")
-		self.sendMessageToClient(serverfd,"BYE")
+		#self.sendMessageToClient(serverfd,"STOPSNIFFING")
+		self.sendMessageToClient(serverfd,"GEMINISHUTDOWN")
 
 
 def main():
@@ -143,7 +219,6 @@ def main():
 	try:
 		while True:
 			data = mygemini_server.recvMessageFromClient(serverfd)
-			print data
 			if data == "HIYA":
 				mygemini_server.sendMessageToClient(serverfd,"STARTSNIFFING")
 			else:
@@ -152,6 +227,8 @@ def main():
 		print "\nProgram aborted ........."
 		print "Shutting down Server ...."
 		mygemini_server.serverClosedownHandshake(serverfd)
+		time.sleep(10)
+		#mygemini_server.sendMessageToClient(serverfd,"STARTSNIFFING")
 
 	mygemini_server.closeServer(serverfd)
 
